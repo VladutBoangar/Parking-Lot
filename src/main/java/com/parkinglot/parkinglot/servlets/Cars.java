601bfa2acddc18a1c7e598eda87b918c66048333
VladutBoangar <vladutoctavian.boangar@ulbsibiu.ro>
2025-11-04 09:04:33 +0200

    Created a Cars servlet and page

--- src/main/java/com/parkinglot/parkinglot/servlets/Cars.java
+++ src/main/java/com/parkinglot/parkinglot/servlets/Cars.java
@@ -11,7 +11,10 @@ public class Cars extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws
             ServletException, IOException {
+        request.setAttribute("numberOfFreeParkingSpots", 10);
+        request.getRequestDispatcher("/WEB-INF/pages/cars.jsp").forward(request,response);
     }
+
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws
